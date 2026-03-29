1a7312be863481dfe3d9b9dac5efb45f4a98c07a
moonwqwizlio <tatsveet@gmail.com>
2026-03-29 19:22:12 +0300

    feat: migrations runs automatically

--- internal/storage/database.go
+++ internal/storage/database.go
@@ -6,23 +6,35 @@ import (
 	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/jackc/pgx/v5/stdlib"
+	"github.com/pressly/goose/v3"
 )
 
 func MustLoadDatabase(databaseURL string) *pgxpool.Pool {
 	ctx := context.Background()
+
 	dbpool, err := pgxpool.New(ctx, databaseURL)
 	if err != nil {
 		log.Fatalf("Unable to create connection pool: %v\n", err)
 	}
 
-	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	pingCtx, cancel := context.WithTimeout(ctx, 5*time.Second)
 	defer cancel()
-
-	err = dbpool.Ping(ctx)
-	if err != nil {
+	if err := dbpool.Ping(pingCtx); err != nil {
 		log.Fatalf("Unable to connect to database: %v\n", err)
 	}
 
-	log.Println("Successfully connected to database!")
+	db := stdlib.OpenDBFromPool(dbpool)
+
+	if err := goose.SetDialect("postgres"); err != nil {
+		log.Fatalf("Failed to set goose dialect: %v", err)
+	}
+
+	log.Println("Running migrations...")
+	if err := goose.Up(db, "migrations"); err != nil {
+		log.Fatalf("Goose migrations failed: %v", err)
+	}
+
+	log.Println("Successfully connected to database and applied migrations!")
 	return dbpool
 }
